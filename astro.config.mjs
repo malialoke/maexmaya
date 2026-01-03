b2bac0205ca6dc3a76efe47e91b81e52c4cdae57
malialoke <malialoke@gmail.com>
2026-01-03 14:46:02 -0800

    More testing

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,8 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
+import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
   site: 'https://your-domain.com',
-  integrations: [react()],
+  integrations: [react(), tailwind()],
 });
